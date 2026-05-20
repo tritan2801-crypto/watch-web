d3eae25432b93b0ee5acf48eab2922142fbafd1b
tritan2801-crypto <tritan2801@gmail.com>
2026-05-20 15:54:49 +0700

    fix: update CORS config to allow all origins

--- server.js
+++ server.js
@@ -1,34 +1,21 @@
 import jsonServer from "json-server";
-import cors from "cors";
 
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
-const allowedOrigins = [
-  "https://watch-web-six.vercel.app",
-  "http://localhost:5173",
-  "http://localhost:3000",
-];
-
-server.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    credentials: true,
-  })
-);
-
 server.use(middlewares);
+
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 server.use(router);
 
 const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`);
+  console.log("JSON Server is running");
 });
