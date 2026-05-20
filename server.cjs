ba5fb9bb3b05150a1f3e8eb2fcfc59dfde526776
tritan2801-crypto <tritan2801@gmail.com>
2026-05-20 16:18:58 +0700

    fix: rename to .cjs, add OPTIONS preflight and Authorization header

---
+++ server.cjs
@@ -0,0 +1,24 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+server.use(router);
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
+});
